b903b5fed1acdb6914c58bf6890add51508eebe4
Hamzah-Amir <hamzaamir7651@gmail.com>
2025-12-23 13:41:11 +0500

    added project urls

--- myfirstsite/home/urls.py
+++ myfirstsite/home/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from home import  views
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('', views.about, name='about'),
-    path('', views.contact, name='contact'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
