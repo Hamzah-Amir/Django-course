ca630373613e794efd061e58f73240e9ec968715
Hamzah-Amir <hamzaamir7651@gmail.com>
2025-12-23 13:31:21 +0500

    initial commit

--- myfirstsite/home/views.py
+++ myfirstsite/home/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("This is home page")
+
+def about(request):
+    return HttpResponse("This is home page")
+
+def contact(request):
+    return HttpResponse("This is home page")
