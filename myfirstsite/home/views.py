5a4e7a50f72720bd422a991d4ce5598f0d605a13
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-01-01 02:32:50 +0500

    written logic to save form data to db

--- myfirstsite/home/views.py
+++ myfirstsite/home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from .models import Contact
 
 # Create your views here.
 
@@ -14,5 +16,14 @@ def about(request):
     # return HttpResponse("This is home page")
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        number = request.POST.get('number')
+        message = request.POST.get('message')
+        date = datetime.today()
+        contact = Contact(name=name, email=email, phone=number, message=message, date=date)
+        contact.save()
+
     return render(request, 'contact.html')
     # return HttpResponse("This is home page")
