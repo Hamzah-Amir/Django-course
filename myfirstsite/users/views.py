4640ccdb1636d6e0e148728710598a17b1331301
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-01-04 14:38:48 +0500

    added views for login page

--- myfirstsite/users/views.py
+++ myfirstsite/users/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def loginPage(request):
+    return render(request, 'users/loginPage.html')
+
+def loginUser(request):
+    pass
+
+def logoutUser(request):
+    pass
