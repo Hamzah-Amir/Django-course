2b5dda78072d89bde35a26a9112d201af80499a6
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-01-04 17:38:43 +0500

    defined login and logout logic

--- myfirstsite/users/views.py
+++ myfirstsite/users/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
 def loginPage(request):
+    print(request.user)
+
     return render(request, 'users/loginPage.html')
 
 def loginUser(request):
-    pass
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            print(request.user.email)
+            return render(request, "users/dashboard.html")
+        else:
+            return render(request, 'users/loginPage.html', {'error': 'Invalid credentials'})
+    
+    return render(request, 'users/loginPage.html')
 
 def logoutUser(request):
-    pass
+    logout(request)
+    return redirect("loginPage")
