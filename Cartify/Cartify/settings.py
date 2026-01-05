0c615a84cfefa6541be27d48a576399bacc4bb67
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-01-05 14:23:28 +0500

    updated settings for external db and custom user model

--- Cartify/Cartify/settings.py
+++ Cartify/Cartify/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import pymysql, os
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,10 +77,16 @@ WSGI_APPLICATION = 'Cartify.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'cartify',
+        'USER': 'root',
+        'PASSWORD': os.environ.get("DATABASE_PASSWORD"),
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 
 # Password validation
