b470d0bd00680cf932837c7f6213a4efe120e4d6
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-01-06 01:47:40 +0500

    completed user registration logic

--- Cartify/users/views.py
+++ Cartify/users/views.py
@@ -1,11 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from users.models import CustomUser
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def role(request):
     return render(request, 'users/role.html')
 
 def registerUser(request):
-    return  render(request, 'users/register.html')
+
+    # Writing backend of authentication
+    role = request.GET.get('role') if request.method == 'GET' else request.POST.get('role')
+    if not role:
+        return redirect('role')
+    
+    if request.method == 'GET':
+        return render(request, 'users/register.html', {'role': role})
+    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        password = request.POST.get('password')
+        gender = request.POST.get('gender')
+        age = request.POST.get('age')
+        phone = request.POST.get('phone')
+        user = CustomUser.objects.create_user(username=username,
+                                               email=email,
+                                               first_name=first_name,
+                                               last_name=last_name,
+                                               password=password,
+                                               role=role,
+                                               gender=gender,
+                                               age=age,
+                                               phone_number=phone)
+        user.save()
+        return redirect('loginUser')
+
+    return redirect('role')
 
 def loginUser(request):
     return render(request, 'users/login.html')
