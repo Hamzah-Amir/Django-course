6a1c1ea1d8fc115238d88291791c341ef8aad0d0
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-01-05 18:48:12 +0500

    added link to static dir

--- Cartify/Cartify/settings.py
+++ Cartify/Cartify/settings.py
@@ -123,3 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Manually Added
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
