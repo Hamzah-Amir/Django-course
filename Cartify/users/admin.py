af414d0a0fca4d50b6a6277be75550e45e059c03
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-01-05 14:25:23 +0500

    registered user model

--- Cartify/users/admin.py
+++ Cartify/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CustomUser
 
 # Register your models here.
+
+admin.site.site_header = "Cartify Admin Panel"
+admin.site.site_title = "Cartify Admin Portal"
+admin.site.index_title = "Welcome to Cartify Admin Panel"
+admin.site.register(CustomUser)
