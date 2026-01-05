98b89dec4264797dc9a9ad505e0e14bb69a64f13
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-01-05 14:23:44 +0500

    defineduser model

--- Cartify/users/models.py
+++ Cartify/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+GENDER_CHOICES = [('Male', 'Male'), ('Female', 'Female'), ('Other', 'Other')]
+class CustomUser(AbstractUser):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(unique=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+    age = models.PositiveIntegerField()
+    phone_number = models.CharField(max_length=15)
+    is_seller = models.BooleanField(default=False)
+    is_buyer = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
