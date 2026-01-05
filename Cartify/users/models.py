d0708fb3ec0e6241f97d697cfefe2c432bb666f8
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-01-05 17:35:25 +0500

    added userid field

--- Cartify/users/models.py
+++ Cartify/users/models.py
@@ -1,16 +1,23 @@
 from django.db import models
+import uuid
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
 GENDER_CHOICES = [('Male', 'Male'), ('Female', 'Female'), ('Other', 'Other')]
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True, null=False, blank=False)
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES, null=True, blank=True)
     age = models.PositiveIntegerField(null=True, blank=True)
     phone_number = models.CharField(max_length=15, null=True, blank=True)
     is_seller = models.BooleanField(default=False)
     is_buyer = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+
