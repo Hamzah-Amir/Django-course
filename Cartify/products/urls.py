0aff9d4e17e3a840320afea3657386f5fc5d10fe
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-01-05 20:40:46 +0500

    creatted urls

--- Cartify/products/urls.py
+++ Cartify/products/urls.py
@@ -2,7 +2,5 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('login', home, name='loginUser'),
-    path('register', home, name='registerUser'),
+    path('', home, name='home')
 ]
