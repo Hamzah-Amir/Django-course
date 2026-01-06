3299213e4cb5344bc6122c68f1916a32bd582a94
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-01-06 23:53:21 +0500

    created view for product detail

--- Cartify/products/views.py
+++ Cartify/products/views.py
@@ -6,10 +6,12 @@ from products.models import Product
 def home(request):
     # Logic for Fetching Products
     products = Product.objects.filter(stock__gt=0)
-            
+
     return render(request, 'products/home.html', {"products": products})
 
-def productDetail(request):
-    if request.method == "GET":
-        uid = request.method.GET.get('uuid')
-    return render(request, 'products/productdetail.html')
+def productDetail(request, uuid):
+    id = uuid
+    product = Product.objects.filter(id=id).first()
+    print(product)
+
+    return render(request, 'products/productdetail.html', {"product": product})
