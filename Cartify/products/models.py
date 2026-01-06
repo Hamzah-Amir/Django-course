cf0b0ed402675123bfbac688cbc5f5ceccd1c36e
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-01-06 17:35:40 +0500

    created products model

--- Cartify/products/models.py
+++ Cartify/products/models.py
@@ -6,9 +6,10 @@ from uuid import uuid4
 class Product(models.Model): 
     seller = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE, related_name='products')
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    serial_number = models.AutoField(unique=True)
     name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='products/')
     description = models.TextField()
+    category = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
