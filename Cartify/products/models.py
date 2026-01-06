897565f246a05f482a98b21ec991a5cffe2f9d76
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-01-06 17:59:32 +0500

    added model for products

--- Cartify/products/models.py
+++ Cartify/products/models.py
@@ -9,10 +9,9 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     image = models.ImageField(upload_to='products/')
     description = models.TextField()
-    category = models.CharField(max_length=100)
+    category = models.CharField(max_length=100, null=False)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
-
     def __str__(self):
         return self.name
