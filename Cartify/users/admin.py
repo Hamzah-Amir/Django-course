0364b481d6c741003d629f8352eddb19063ed27f
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-01-26 20:56:34 +0500

    added wishlist model

--- Cartify/users/admin.py
+++ Cartify/users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, WishlistItem
 
 # Register your models here.
 
@@ -7,3 +7,4 @@ admin.site.site_header = "Cartify Admin Panel"
 admin.site.site_title = "Cartify Admin Portal"
 admin.site.index_title = "Welcome to Cartify Admin Panel"
 admin.site.register(CustomUser)
+admin.site.register(WishlistItem)
