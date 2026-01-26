7bfa9028aaca91894ffaaad1b911f5454497eea3
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-01-26 20:58:32 +0500

    created view structure od seller central

--- Cartify/seller/views.py
+++ Cartify/seller/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def seller(request):
+    if request.user.is_anonymous:
+        return redirect('loginUser')
     return render(request, 'seller/seller_dashboard.html')
