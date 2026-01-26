c88f3073b66cce3e94cd83eab45339a7e071a02f
Hamzah-Amir <hamzaamir7651@gmail.com>
2026-01-26 21:00:06 +0500

    created models

--- Cartify/users/models.py
+++ Cartify/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 import uuid
 from django.contrib.auth.models import AbstractUser
+from products.models import Product
 from datetime import datetime
 
 # Create your models here.
@@ -10,6 +11,7 @@ ROLE_CHOICES = [('buyer', 'Buyer'), ('seller', 'Seller')]
 class CustomUser(AbstractUser):
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(unique=True, null=False, blank=False)
+    profile_image = models.ImageField(upload_to='users/', default='uunn')
     first_name = models.CharField(max_length=30, null=True, blank=True)
     last_name = models.CharField(max_length=30, null=True, blank=True)
     gender = models.CharField(max_length=10, choices=GENDER_CHOICES, null=True, blank=True)
@@ -24,3 +26,10 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
+
+class WishlistItem(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    
+    class Meta:
+        unique_together = ('user', 'product')
